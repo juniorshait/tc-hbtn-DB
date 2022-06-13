0edfe6c2c868db4717509be7e4073ab3574a210b
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-06-13 16:13:10 -0300

    Add files via upload

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE cliente (ID INTEGER PRIMARY KEY autoincrement, nome CHAR(50) NOT NULL, idade INTEGER NOT NULL, cpf CHAR(50) NOT NULL, rg CHAR(50) NOT NULL);
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
