99c02c54cbfd74d0467ba7276ec86cd966e8251c
Shait Moreira <54985510+juniorshait@users.noreply.github.com>
2022-06-13 10:06:39 -0300

    Add files via upload

--- 0x01/java_jdbc/PrintJDBCDrivers.java
+++ 0x01/java_jdbc/PrintJDBCDrivers.java
@@ -1,25 +1,7 @@
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.util.Enumeration;
-
+import java.sql.*;
 public class PrintJDBCDrivers {
-    public static void main(String[] args) {
-
-        System.out.println("Lista de drivers JDBC carregados: ");
-        for (Enumeration<Driver> e = DriverManager.getDrivers(); e.hasMoreElements();) {
-            Driver driver = e.nextElement();
-            dadosDoDriver(driver);
-        }
-    }
-
-    public static void dadosDoDriver(Driver driver) {
-        String nomeClasse = driver.getClass().getName();
-        int maiorVersao = driver.getMajorVersion();
-        int menorVersao = driver.getMinorVersion();
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.println("Nome do driver: " + nomeClasse);
-        System.out.println("Versão mais recente do driver: " + maiorVersao);
-        System.out.println("Versão mais antiga do driver: " + menorVersao);
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        ConnectionSQLite connectionSQLite = new ConnectionSQLite();
+        connectionSQLite.connect();
     }
 }
